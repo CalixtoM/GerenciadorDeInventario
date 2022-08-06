4df45c94c56af61831ce0c519e09a13386e43f17
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2022-08-06 13:16:52 -0300

    loops para controlar numero de produtos a ser add

--- src/inventario/ProductTester.java
+++ src/inventario/ProductTester.java
@@ -6,31 +6,37 @@ import java.util.Scanner;
 public class ProductTester {
 
 	public static void main(String[] args) {
+		int maxSize = 0;
 		Scanner scanProd = new Scanner(System.in);
-		System.out.println("Digite o nome do produto: ");
-		String nome = scanProd.nextLine();
-		System.out.println("Digite o codigo: ");
-		int cod = scanProd.nextInt();
-		System.out.println("Digite a quantidade: ");
-		int qnt = scanProd.nextInt();
-		System.out.println("Digite o valor: ");
-		double valor = scanProd.nextDouble();
-		System.out.println("Esse item estará ativo? \n 1 - Sim, 2 - Não");
-		int atividade = scanProd.nextInt();
-		Produto p1 = new Produto(nome, cod, qnt, valor, atividade);
+		do {
+			if(maxSize < 0) {
+				System.out.print("Valor incorreto inserido \n \n");
+			}
+			System.out.println("Digite quantos produtos deseja adicionar: \n \n Digite 0 para não adicionar nenhum.");
+			maxSize = scanProd.nextInt();
+		}while(maxSize < 0);
 		scanProd.nextLine();
-		System.out.println("Digite o nome do produto: ");
-		nome = scanProd.nextLine();
-		System.out.println("Digite o codigo: ");
-		cod = scanProd.nextInt();
-		System.out.println("Digite a quantidade: ");
-		qnt = scanProd.nextInt();
-		System.out.println("Digite o valor: ");
-		valor = scanProd.nextDouble();
-		System.out.println("Esse item estará ativo? \n 1 - Sim, 2 - Não");
-		atividade = scanProd.nextInt();
-		Produto p2 = new Produto(nome, cod, qnt, valor, atividade);
-		System.out.println(p1+"\n \n"+ p2);
+		if(maxSize == 0) {
+			System.out.print("Nenhum produto será adicionado");
+		}else {
+			Produto[] p = new Produto[maxSize];
+			for(int i = 0; i < maxSize; i++) {
+				System.out.println("Digite o nome do produto: ");
+				String nome = scanProd.nextLine();
+				System.out.println("Digite o codigo: ");
+				int cod = scanProd.nextInt();
+				System.out.println("Digite a quantidade: ");
+				int qnt = scanProd.nextInt();
+				System.out.println("Digite o valor: ");
+				double valor = scanProd.nextDouble();
+				System.out.println("Esse item estará ativo? \n 1 - Sim, 2 - Não");
+				int atividade = scanProd.nextInt();
+				p[i] = new Produto(nome, cod, qnt, valor, atividade);
+				scanProd.nextLine();
+				System.out.println(p[i]+"\n \n");
+			}
+		}
+		
 		
 	}
 
